71246bfa184e3c185f60e5a771991e4647c03048
Aécio Santos <aecio.solando@gmail.com>
2021-11-01 23:50:40 -0400

    Setup gradle application plugin

---
+++ benchmark/src/main/java/corrsketches/benchmark/Main.java
@@ -0,0 +1,17 @@
+package corrsketches.benchmark;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+    subcommands = {
+      ComputePairwiseJoinCorrelations.class,
+      CreateColumnStore.class,
+      ComputeBudget.class
+    })
+public class Main {
+
+  public static void main(String[] args) {
+    System.exit(new CommandLine(new Main()).execute(args));
+  }
+}
