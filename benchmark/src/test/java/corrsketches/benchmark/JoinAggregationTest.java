e1237b2d0183cec8d08463f82def8641d9e9ce25
Aécio Santos <aecio.solando@gmail.com>
2021-12-01 18:36:50 -0500

    Rename TablesTest class to JoinAggregationTest

--- benchmark/src/test/java/corrsketches/benchmark/JoinAggregationTest.java
+++ benchmark/src/test/java/corrsketches/benchmark/JoinAggregationTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TablesTest {
+public class JoinAggregationTest {
 
   @Test
   public void shouldAggregateColumnPairWithRepeatedKeys() {
