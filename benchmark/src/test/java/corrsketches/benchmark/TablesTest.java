022fa7455bddd316baf6f0ec70fc270c26be737d
Aécio Santos <aecio.solando@gmail.com>
2023-10-09 11:02:46 -0400

    Change paths in TablesTest to be OS-independent

--- benchmark/src/test/java/corrsketches/benchmark/TablesTest.java
+++ benchmark/src/test/java/corrsketches/benchmark/TablesTest.java
@@ -2,7 +2,8 @@ package corrsketches.benchmark;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,8 @@ import org.junit.jupiter.api.Test;
 public class TablesTest {
 
   @Test
-  public void shouldFindCSVFiles() throws IOException {
-    String directory = TablesTest.class.getResource("TablesTest/csv-files/").getPath();
+  public void shouldFindCSVFiles() throws Exception {
+    String directory = resolvePath("TablesTest/csv-files/");
     final List<String> allCSVs = Tables.findAllTables(directory);
 
     assertThat(allCSVs).singleElement().isNotNull();
@@ -19,8 +20,8 @@ public class TablesTest {
   }
 
   @Test
-  public void shouldReadCSVFileColumnPairs() {
-    String csvFile = TablesTest.class.getResource("TablesTest/csv-files/test1.csv").getPath();
+  public void shouldReadCSVFileColumnPairs() throws Exception {
+    String csvFile = resolvePath("TablesTest/csv-files/test1.csv");
     final Iterator<ColumnPair> it = Tables.readColumnPairs(csvFile, 0);
     assertThat(it.hasNext()).isTrue();
 
@@ -30,22 +31,26 @@ public class TablesTest {
   }
 
   @Test
-  public void shouldFindParquetFiles() throws IOException {
-    String directory = TablesTest.class.getResource("TablesTest/parquet-files/").getPath();
+  public void shouldFindParquetFiles() throws Exception {
+    String directory = resolvePath("TablesTest/parquet-files/");
     final List<String> allCSVs = Tables.findAllTables(directory);
     assertThat(allCSVs).singleElement().isNotNull();
     assertThat(allCSVs.get(0)).endsWith("test1.parquet");
   }
 
   @Test
-  public void shouldReadParquetFileColumnPairs() {
-    String csvFile =
-        TablesTest.class.getResource("TablesTest/parquet-files/test1.parquet").getPath();
+  public void shouldReadParquetFileColumnPairs() throws Exception {
+    String csvFile = resolvePath("TablesTest/parquet-files/test1.parquet");
     final Iterator<ColumnPair> it = Tables.readColumnPairs(csvFile, 0);
     assertThat(it.hasNext()).isTrue();
 
     ColumnPair cp = it.next();
     assertThat(cp.keyName).isEqualTo("key");
     assertThat(cp.columnName).isEqualTo("value");
+  }
+
+  /** Resolves paths in OS-independent way. */
+  private static String resolvePath(String path) throws URISyntaxException {
+    return Paths.get(TablesTest.class.getResource(path).toURI()).toString();
   }
 }
